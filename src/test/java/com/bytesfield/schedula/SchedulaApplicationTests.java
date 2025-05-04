70079b23272dbe5cc7fd5dffe22d1c8b6bca081b
Abraham Udele <abrahamudele@gmail.com>
2025-05-04 01:18:55 +0100

    first commit

---
+++ src/test/java/com/bytesfield/schedula/SchedulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytesfield.schedula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
